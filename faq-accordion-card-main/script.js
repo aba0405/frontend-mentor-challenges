59da3118d28fd1b15fa3dd201e71a6eb82d32da9
aba0405 <adnan.abdal@gmail.com>
2021-07-07 09:35:03 -0400

    Work in Progress

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,1 +1,12 @@
-console.log("Test");
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("click", (e) => {
+    input.forEach((input) => {
+      if (input.checked) {
+        input.checked = false;
+        e.target.checked = true;
+      }
+    });
+  });
+});
